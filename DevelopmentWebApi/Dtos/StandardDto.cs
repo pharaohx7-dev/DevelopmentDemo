d7c623efdca59cc24957d32afa07df0c4f6af285
pharaohx7-dev <54245731+pharaohx7-dev@users.noreply.github.com>
2019-08-25 12:36:43 -0600

    Add files via upload

---
+++ DevelopmentWebApi/Dtos/StandardDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevelopmentWebApi._Common.Dtos
+{
+    public class StandardDto
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
