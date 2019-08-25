1567dbc9f389a022099a020c4ed6be2d1bc273e0
pharaohx7-dev <54245731+pharaohx7-dev@users.noreply.github.com>
2019-08-25 12:32:33 -0600

    API: Carga Inicial

---
+++ DevelopmentWebApi/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevelopmentInfrastructure;
+using DevelopmentWebApi.Desarrollo.AppServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+
+namespace DevelopmentWebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<DevelopmentContext>();
+            services.AddTransient<ProjectsAppService>();
+
+            services.AddCors(o => o.AddPolicy("AllowAnyOrigin", builder =>
+            {
+                builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+            }));
+
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver =
+                    new CamelCasePropertyNamesContractResolver();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("AllowAnyOrigin");
+
+            app.UseMvc();
+        }
+    }
+}
